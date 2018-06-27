d52e359fc2372fbd12e418278203484d5da4cc7d
carolinebyron <byroncaroline2@gmail.com>
2018-06-27 14:46:49 +0000

    automatically backed up by learn

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,3 +1,9 @@
-class Model
-  # Replace with Dog clas
+def get_fortune
+  
+  fortune_array= ["you'll find your true love", "you will get poured on by a weird acid and become a superhero", "You'll fall in a hole in 2 days"]
+  return = fortune_array.sample
+  fortune_array.delete(fortune)
+  return fortune 
+  
+  
 end
