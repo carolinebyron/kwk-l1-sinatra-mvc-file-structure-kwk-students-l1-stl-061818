5a1c9a385831b292b1509acf872889fd76184002
carolinebyron <byroncaroline2@gmail.com>
2018-06-27 14:50:01 +0000

    automatically backed up by learn

--- app/models/model.rb
+++ app/models/model.rb
@@ -2,8 +2,5 @@ def get_fortune
   
   fortune_array= ["you'll find your true love", "you will get poured on by a weird acid and become a superhero", "You'll fall in a hole in 2 days"]
   return = fortune_array.sample
-  fortune_array.delete(fortune)
-  return fortune 
-  
-  
 end
+puts get_fortune
