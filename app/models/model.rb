6567a5595558be8dfcfe674fc9920ef6ca6e4954
carolinebyron <byroncaroline2@gmail.com>
2018-06-27 14:51:56 +0000

    automatically backed up by learn

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,6 +1,6 @@
 def get_fortune
   
   fortune_array= ["you'll find your true love", "you will get poured on by a weird acid and become a superhero", "You'll fall in a hole in 2 days"]
-  return = fortune_array.sample
+  return fortune_array.sample
 end
 puts get_fortune
